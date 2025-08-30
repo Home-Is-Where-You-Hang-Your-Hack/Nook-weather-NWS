3aa33561edba06d22b633a0e03d50971200c248b
Tom Hiller <thrilleratplay@gmail.com>
2025-08-30 17:27:57 -0400

    fix: catch invalid icon URL

--- src/util/helpers/mapNwsIconMap.helper.ts
+++ src/util/helpers/mapNwsIconMap.helper.ts
@@ -9,22 +9,26 @@ import type { INwsIconDayNightMap } from 'types/weather.js';
  * @returns updated icon path
  */
 const mapNwsIconMap = (nwsIcon: string | undefined): string | null => {
-  const { pathname } = new URL(nwsIcon ?? '');
-  if (!nwsIcon || !pathname) {
+  try {
+    const { pathname } = new URL(nwsIcon ?? '');
+    if (!nwsIcon || !pathname) {
+      return null;
+    }
+    const urlPath: string[] = pathname.split('/');
+    const condition: string | null = urlPath[urlPath.length - 1]?.split(',')[0] ?? null;
+    const isDaytime: boolean = urlPath.some((x) => x === 'day');
+    const iconMap: INwsIconDayNightMap | null = NWS_WEATHER_ICON_MAP[condition ?? ''] ?? null;
+
+    if (!iconMap) {
+      return null;
+    }
+
+    const wiIcon = (!isDaytime && iconMap.night) ? iconMap.night : iconMap.day;
+
+    return `/png/${wiIcon}.png`;
+  } catch {
     return null;
   }
-  const urlPath: string[] = pathname.split('/');
-  const condition: string | null = urlPath[urlPath.length - 1]?.split(',')[0] ?? null;
-  const isDaytime: boolean = urlPath.some((x) => x === 'day');
-  const iconMap: INwsIconDayNightMap | null = NWS_WEATHER_ICON_MAP[condition ?? ''] ?? null;
-
-  if (!iconMap) {
-    return '';
-  }
-
-  const wiIcon = (!isDaytime && iconMap.night) ? iconMap.night : iconMap.day;
-
-  return `/png/${wiIcon}.png`;
 };
 
 export default mapNwsIconMap;
